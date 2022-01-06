86ac0448e0bd9fa2fa8e5c58f88ad0ffb8360a93
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2022-01-06 12:06:58 +0900

    :art: Construct to Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+    <board-login></board-login>
+    <board-register></board-register>
   </div>
 </template>
 
 <script>
-import AppHeader from "./components/AppHeader.vue";
+import BoardLogin from "./components/auth/BoardLogin.vue";
+import BoardRegister from "./components/auth/BoardRegister.vue";
 
 export default {
-  data: function () {
-    return {
-      str: "hi",
-    };
-  },
   components: {
-    "app-header": AppHeader,
-  },
-  methods: {
-    renewStr: function () {
-      this.str = "header";
-    },
+    BoardLogin: BoardLogin,
+    BoardRegister: BoardRegister,
   },
 };
 </script>
