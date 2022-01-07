2609d26f39b095b61674f7e68709d6c70d174e29
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2022-01-07 18:00:28 +0900

    :art: Setting to Eslint

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div>
+  <div id="app">
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+
     <board-login></board-login>
-    <board-register></board-register>
+    <board-sign-up></board-sign-up>
+    <board-write></board-write>
   </div>
 </template>
 
 <script>
 import BoardLogin from "./components/auth/BoardLogin.vue";
-import BoardRegister from "./components/auth/BoardRegister.vue";
+import BoardSignUp from "./components/auth/BoardSignUp.vue";
+import BoardWrite from "./components/write/BoardWrite.vue";
 
 export default {
   components: {
-    BoardLogin: BoardLogin,
-    BoardRegister: BoardRegister,
+    BoardLogin,
+    BoardSignUp,
+    BoardWrite,
   },
+  props: {},
 };
 </script>
 
