3006da65cf9a2880d174f5360e80f8de7feeb8ec
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2022-01-10 18:54:29 +0900

    :art: Construct LoadingSpinner

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">전체 포스트</h1>
-      <ul>
+      <loading-spinner v-if="isLoading"></loading-spinner>
+      <ul v-else>
         <post-list-item
           v-for="postItem in postItems"
           :key="postItem.seq"
@@ -15,20 +16,25 @@
 
 <script>
 import PostListItem from "../components/boards/BoardListItem.vue";
+import LoadingSpinner from "../components/common/LoadingSpinner.vue";
 import { fetchPosts } from "../api/index";
 export default {
   components: {
     PostListItem,
+    LoadingSpinner,
   },
 
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       this.postItems = data.data.list;
     },
   },
